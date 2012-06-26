8051dd641dc39c36b66c5e640bce25625f146bfe
Will P <beiju@iwillsite.com>
2012-06-25 23:56:42 -0700

    Made images extensible Added simple error handling

--- popover.js
+++ popover.js
@@ -3,44 +3,56 @@
 safari.extension.settings.addEventListener("change",settingsChange,false);
 safari.self.addEventListener("message", handleMessage, false)
 
-var bookmarklets = JSON.parse(safari.extension.settings.bookmarklets.replace(/[\u201C\u201D\u201E\u201F\u2033\u2036\u2018\u2019\u201A\u201B\u2032\u2035]/gi, '"')),
-		shortcuts = {};
+try {
+	var bookmarklets = JSON.parse(safari.extension.settings.bookmarklets.replace(/[\u201C\u201D\u201E\u201F\u2033\u2036\u2018\u2019\u201A\u201B\u2032\u2035]/gi, '"')),
+			shortcuts = {};
 
 
-function createBkm(name, url, shortcut, num) {
-	var li = document.createElement('li');
-	li.id = name;
-	li.innerHTML = '<a href="'+url+'"><img src="icons/'+name+'.png" height="57", width="57"><p>'+shortcut+' &middot; '+num+'</p></a>';
-	bkmlist.appendChild(li)
-}
-
-_.each(bookmarklets, function(bkm, i) {
-	createBkm(bkm.name, bkm.url, bkm.shortcut, i+1);
-	shortcuts[bkm.shortcut] = i;
-});
-safari.self.width = 87*bookmarklets.length + 16;
-
-document.addEventListener('keypress', function(event) {
-	var key = String.fromCharCode(event.charCode), i;
-	if (key.match(/[1-9]/g)) {
-		i = parseInt(key, 10) - 1;
-	} else if (_.has(shortcuts, key)) {
-		i = shortcuts[key];
-	} else {
-		return false;
+	function createBkm(bkm, num) {
+		var li = document.createElement('li');
+		li.id = bkm.name;
+		li.innerHTML = '<a href="'+bkm.url+'"><div class="image" style="background-image: url('+bkm.icon+');"></div><p>'+bkm.shortcut+' &middot; '+num+'</p></a>';
+		bkmlist.appendChild(li)
 	}
 	
-	var elem = document.getElementById(bookmarklets[i].name).getElementsByTagName('a')[0];
-	trigger(elem);	
-	event.preventDefault();
-}, false);
-
-_.each(document.getElementsByTagName('a'),  function(a) {
-	a.addEventListener('click', function(event) {
-		trigger(this);
+	_.each(bookmarklets, function(bkm, i) {
+		createBkm(bkm, i+1);
+		shortcuts[bkm.shortcut] = i;
+	});
+	safari.self.width = 87*bookmarklets.length + 16;
+	
+	document.addEventListener('keypress', function(event) {
+		var key = String.fromCharCode(event.charCode), i;
+		if (key.match(/[1-9]/g)) {
+			i = parseInt(key, 10) - 1;
+		} else if (_.has(shortcuts, key)) {
+			i = shortcuts[key];
+		} else {
+			return false;
+		}
+		
+		var elem = document.getElementById(bookmarklets[i].name).getElementsByTagName('a')[0];
+		trigger(elem);	
 		event.preventDefault();
-	}, false)
-});
+	}, false);
+	
+	_.each(document.getElementsByTagName('a'),  function(a) {
+		a.addEventListener('click', function(event) {
+			trigger(this);
+			event.preventDefault();
+		}, false)
+	});
+
+} catch (e) {
+	var error = document.createElement('div');
+	error.className = 'error';
+	error.innerHTML = '<h3>'+e.name+'</h3><p>'+e.message+'</p>';
+	bkmlist.appendChild(error);
+	safari.self.width = 300;
+	safari.self.height = error.clientHeight + 4;
+window.setTimeout(function() {
+			}, 1000);
+}
 
 function settingsChange() {
 	window.location.reload()
